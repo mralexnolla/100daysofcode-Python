c461664ddc60d14e1977cd0e852374a5a75117b1
alex <nba4everwanted@gmail.com>
2023-04-19 13:42:51 +0000

    day3 controle flow and logical operations

---
+++ day3/main.py
@@ -0,0 +1,47 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+print("Welcome to the treasure island, your mission is to find the treasure");
+
+direction = (input("Choose a direction 'left' or right ?")).lower();
+if direction == "right":
+  print("Game over. You have jumped off the cliff");
+elif direction == "left":
+  decision = (input("Do you want to 'swim' or 'wait' ?")).lower();
+  if decision == "swim":
+    print("Game over. You have been attacked by a see snake");
+  elif decision == "wait":
+    doorcolor = (input("Which will you want to open 'Blue', 'Yellow' or 'Red' ?")).lower();
+    if doorcolor == 'blue':
+      print("Game over. You have been eaten by a beast");
+    elif doorcolor == 'red':
+      print("Game over. You have been burned by fire")
+    elif doorcolor == 'yellow':
+      print("you win")
+
+
